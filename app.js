b4bbc3b45d81ecf2330001684ec118751d73586b
zainaejaz <zainaijaz32@gmail.com>
2024-05-15 22:25:19 +0500

    second  commit

---
+++ app.js
@@ -0,0 +1,43 @@
+const movData = document.getElementById("modal");
+const input = document.getElementById("searchMov");
+const searcBtn = document.getElementById("searcBtn");
+
+async function getMovie(movie) {
+  const res = await fetch(
+    `https://www.omdbapi.com/?t=${movie}&apikey=9b010102`
+  );
+
+  const data = await res.json();
+
+  if (data.Response === "True") {
+    movData.innerHTML = `<div class="m-3 pt-2 container movBox bg-color">
+        <div class="post-img">
+          <img src=${data.Poster} class="rounded" alt="nothing">
+        </div>
+        <div>
+          <ul class="mt-5 mb-5 text" style="background-color: #700e0e">
+            <li>Title: ${data.Title}:</li>
+            <li>Released: ${data.Released}</li>
+            <li>Runtime: ${data.Runtime}</li>
+            <li>Genre: ${data.Genre}</li>
+            <li>Actors: ${data.Actors}</li>
+            <li>Plot: ${data.Plot}</li>
+            <li>Language: ${data.Language}</li>
+          </ul>
+        
+        </div>
+        </div>`;
+  } else {
+    movData.innerHTML = `
+        <div class="container mt-5">
+          <h1 class="errMsg">Movie not found</h1>
+        </div>
+  `;
+  }
+}
+
+searcBtn.addEventListener("click", function (e) {
+  e.preventDefault();
+  let getData = input.value;
+  getMovie(getData);
+});
---
+++ readme.md
@@ -0,0 +1,3 @@
+#MovieShowCase
+
+MovieShowCase is a modern movie website designed to be your ultimate destination for all things related to movies. With a user-friendly interface and intuitive design, Cinemania offers a seamless browsing experience for movie.
